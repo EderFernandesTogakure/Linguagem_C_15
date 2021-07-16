cd8c11a8278e47a419b64ac85374c9cbc79faed2
Eder Carlos Fernandes <eder.tecnologias@gmail.com>
2021-07-16 10:39:13 -0300

    Add files via upload

---
+++ Aula15_4/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+     int a, b;
+
+    printf("Digite o valor de A!\n");
+    scanf("%d",&a);
+    printf("Digite o valor de B!\n");
+    scanf("%d",&b);
+    while(a < b && a > 0){
+        printf("O valor e %d \n", a);
+        a ++;
+    }
+}
---
+++ Aula_15_1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int contador = 0;
+
+    while(contador < 5){
+      printf("Contador = %d\n", contador);
+      contador += 1;
+    }
+    printf("Acabou!");
+
+}
